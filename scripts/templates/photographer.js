7cdb0f5e1bac7f510ce2f4d70467cb0283a99927
cedrick keumaleu <keumaleucedrick@gmail.com>
2023-06-20 09:34:23 +0200

    début du projet

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,28 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city,country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        // varible pour récupérer la photo de profil
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+    
+        // variable pour recupérer le nom du photographe
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        // variable pour recupérer le pays
+        const p = document.createElement('p')
+        p.textContent = city,
+        p.textContent = country,
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p)
         return (article);
     }
     return { name, picture, getUserCardDOM }
+
+    
 }
+
