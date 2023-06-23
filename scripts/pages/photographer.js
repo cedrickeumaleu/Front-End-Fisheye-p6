95630159fae4ef22a55e2c39c3291498ea8a186f
cedrick keumaleu <keumaleucedrick@gmail.com>
2023-06-23 21:23:58 +0200

    fin de la page d'accueil et début page photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,3 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Mettre le code JavaScript lié à la page photographer.htmt
+https://example.com/?nom=Jonathan%20Smith&age=18
+a.href = `photographer.html?id=${id}`
