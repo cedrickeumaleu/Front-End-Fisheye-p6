ff767b44cbf487239300ab209ef060f174f45b41
cedrick keumaleu <keumaleucedrick@gmail.com>
2023-07-25 17:42:11 +0200

    affichage de media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,7 +7,6 @@ async function getPagePhotographers() {
    if (photographersPage.ok === true){
      // et bien retourner le tableau photographers seulement une fois récupéré
     return photographersPage.json();
-
    }
    throw new Error('impossible de contacter le serveur')
       
@@ -16,7 +15,6 @@ async function getPagePhotographers() {
 //resultat:en fontion d'un id, renvoie un objet avec les info du photographe
 async function getPhotographerById(id){
     const photographersData = await getPagePhotographers()
-    
     let findPhotographer;
     photographersData.photographers.forEach((photographer) =>{
         
@@ -27,6 +25,7 @@ async function getPhotographerById(id){
     })
     return findPhotographer
 }
+
 async function init(){
     const params = (new URL(document.location)).searchParams;
     const id = params.get('id'); 
@@ -40,12 +39,52 @@ init()
 // //récupération du travail de chaque photographes
 async function displayData(photographer) {
     // recupération du header photographers.html
-
+    const photographersData = await getPagePhotographers()
     const photographerModel = photographerTemplate(photographer);
-          photographerModel.getPageDOM();
-    
-   
+          photographerModel.getPageDOM(); // affichage de la modele photographer
+
+    const media = getMediasByPhotographerId(photographer.id)         
+    const mediaModel = mediaTemplate(media, photographer.name)
+          mediaModel.getImageDOM(); // affichage de la modale media
+          
+    let namePhotographer = []; 
+        photographersData.media.forEach((m) =>{
+        namePhotographer.push(mediaTemplate(m, photographer.name))
+       
+    })
+    return namePhotographer;
+
 }
+
+
+//resultat:en fontion de photographerId, renvoie un objet avec les info du media de chaque photographe
+async function getMediasByPhotographerId(photographerId){
+    const photographersData = await getPagePhotographers()
+    
+    let findMedias =[];
+    photographersData.media.forEach((media) =>{
+        
+       if (media.photographerId == photographerId){
+        findMedias.push(media)
+        
+       }
+    })
+    return findMedias
+}
+
+async function getMediasByPhotographerId(){
+    const photographersData = await getPagePhotographers()
+
+    let imageMedias = [];
+    photographersData.media.forEach((image) =>{
+        imageMedias.push(mediaTemplate(image))
+    
+    })
+    return imageMedias;
+}
+
+
+
 
 
 
